77c40ea485e0d7377f17d3112824ae848747dc19
mkrecny <mkrecny@gmail.com>
2011-03-06 14:12:37 -0500

    added expanded urls for t.co

--- twitter/test/addbackfill.js
+++ twitter/test/addbackfill.js
@@ -6,18 +6,18 @@ var util = require('../../common/util.js');
 var bs = require('../../common/beanstalk_client.js').Client();
 //.........//
 
-var message = {
-  "action":"add_user", 
-  "twitter_id":(Math.floor(Math.random() * (5000 - 1000 + 1) + 1000)),
-  "oauth_token":"232439599-8Ji6In1HvmOuXiYh24iCA6kYasGfPMiS96FgPMCV",
-  "oauth_secret":"wOSnCeZqXJYQXXOZ4PVtmHAICR9WUvJTbY6e9mKLkQ"
+var message = 
+  {
+    "action":"add_user", 
+    "twitter_id":(Math.floor(Math.random() * (5000 - 1000 + 1) + 1000)),
+    "oauth_token":"232439599-8Ji6In1HvmOuXiYh24iCA6kYasGfPMiS96FgPMCV",
+    "oauth_secret":"wOSnCeZqXJYQXXOZ4PVtmHAICR9WUvJTbY6e9mKLkQ"
   };
 
 bs.use(config.twitter_backfill_tube).onSuccess(function(data) 
 {
 	bs.put(JSON.stringify(message)).onSuccess(function(data)
-	{ 
-	    util.log(data);
+	{
 	  	bs.disconnect();
 	});
 });
--- twitter/twitter.backfill.js
+++ twitter/twitter.backfill.js
@@ -17,32 +17,33 @@ function backfillUser(twit_client, twitter_user_id, job_id)
   var page_counter = 0;
   
   for(var p=page_start; p>0; p-=1)
-  { util.log({status:'retrieving pages', type:'backfill', job_id:job_id, twitter_id:twitter_user_id})
+  { 
+    util.log({status:'retrieving pages', type:'backfill', job_id:job_id, twitter_id:twitter_user_id})
     getPage(twit_client, p, function(page)
     {
       page_counter+=1;
       if (page_counter==17) 
       {
-	twit_client = null; //little iffy about this one. garbo won't collect til all refs are nullified
+	      twit_client = null; //does this actually work?
         util.log({status:'all pages retrieved', type:'backfill', job_id:job_id, twitter_id:twitter_user_id});
         job_manager.deleteJob(job_id, function(job_del_data)
         {
-	  listen();
+	        listen();
         });
       }
       getPageLinks(page, function(link, tweet, linkCallback)
       { 
         var job_spec =
         {
-	 "twitter_status_update":tweet,
-         "url":link,
-         "provider_type":"twitter",
-         "provider_user_id":twitter_user_id
+	         "twitter_status_update":tweet,
+           "url":link,
+           "provider_type":"twitter",
+           "provider_user_id":twitter_user_id
         };
         
         job_manager.addJob(config.twitter_link_tube, job_spec, function(job_data)
         {
-	  util.log({status:'link proccess job added', url:job_spec.url, type:'backfill>>link_processing', job_id:job_data}); 
+	        util.log({status:'link proccess job added', url:job_spec.url, type:'backfill>>link_processing', job_id:job_data}); 
         });
       });
     });
@@ -65,8 +66,9 @@ function getPageLinks(page, linkExtractedCallback)
     if (page[i] && page[i].entities && page[i].entities.urls)
     {
       for (var u in page[i].entities.urls)
-      {
-        linkExtractedCallback(page[i].entities.urls[u].url, page[i]);
+      { 
+        var url = page[i].entities.urls[u].expanded_url ? page[i].entities.urls[u].expanded_url : page[i].entities.urls[u].url;
+        linkExtractedCallback(url, page[i]);
       }
     }
   }
