c90336ef5aa3113591dff2b5d65d9510bbcfbab2
mkrecny <mkrecny@gmail.com>
2011-03-10 20:04:34 -0500

    simplest verion

--- twitter/simple.stream.js
+++ twitter/simple.stream.js
@@ -10,33 +10,9 @@ var redis = require('redis').createClient(config.redis_config.port, config.redis
 var job_manager = require('../common/job.manager.js');
 var twit = new twitter(config.twitter_keys);
 
-function parseSiteStreamTweet(tweet)
+function parseSiteStreamTweet(data)
 {
-  var urls = null;
-  if (tweet.message.entities && tweet.message.entities.urls && tweet.message.entities.urls.length)
-  {
-    urls = tweet.message.entities.urls;
-    for (var u in urls)
-    { 
-      var url = urls[u].expanded_url ? urls[u].expanded_url : urls[u].url;
-      util.expandURL(url, tweet.message, function(expanded, tweet_msg)
-      {
-        var job_spec =
-        {
-	         "twitter_status_update":tweet_msg,
-           "url":expanded,
-           "provider_type":"twitter",
-           "provider_user_id":tweet.for_user
-        };
-        
-        job_manager.addJob(config.twitter_link_tube_add, job_spec, function(job_data)
-        {
-	        //util.log({status:'link proccess job added', url:job_spec.url, type:'stream', job_id:job_data}); 
-        });    
-      });
-    }
-  }
-  
+  console.log('TWEET!');
 }
 
 twit.stream('site', {follow:[251386798], with:'followings'}, function(stream)
