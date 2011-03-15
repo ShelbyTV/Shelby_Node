79a436a129a3c9468a8714a4b1d710b53a0d7477
mkrecny <mkrecny@gmail.com>
2011-03-15 15:00:57 -0400

    changing user - ip combination

--- twitter/test/addbackfill.js
+++ twitter/test/addbackfill.js
@@ -10,8 +10,8 @@ var message = //this is a sample job - and the key names need to change for twit
 {
   "action":"add_user", 
   "twitter_id":(Math.floor(Math.random() * (5000 - 1000 + 1) + 1000)),
-  "oauth_token":"251386798-4hoHpv0Hs541ZAr4apf7tkvUXRmqOWRU1xUkYbui",
-  "oauth_secret":"vv8TjLFxICn1MM1KZ93pvWE5MYbpBguNnxmMKCDp0"
+  "oauth_token":"232439599-MrVUudnONywl0a1KY9XEYwDH3neNv1ESp4l8OU",
+  "oauth_secret":"Cio44HxikuDcJM7UvkhlXL3zVCSFrjZ80I7gZikToaw"
 };
 
 bs.use(config.twitter_backfill_tube).onSuccess(function(data) 
