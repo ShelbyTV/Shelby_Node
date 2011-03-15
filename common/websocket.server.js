209e5ca3ac0ecc39197668b2b41d4c553077d042
mkrecny <mkrecny@gmail.com>
2011-03-15 12:36:38 -0400

    finish testing mode for websocket

--- common/websocket.server.js
+++ common/websocket.server.js
@@ -78,7 +78,7 @@ function pushPayloadToClient(payload, user_id, callback)
  for (var i in Clients[user_id])
  {
    util.log({"status":"sending payload to user", "client":user_id, "payload":payload})
-   response.sendMessageToClient(payload, Clients[user_id][i]);
+   Clients[user_id][i].send(payload);
    completed+=1;
    if (completed===Clients[user_id].length)
    {
