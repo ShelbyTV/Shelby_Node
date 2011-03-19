9aac447c2ac378410d26ee927d202248130a6ab9
mkrecny <mkrecny@gmail.com>
2011-03-19 18:01:57 -0400

    job king uses one connection

--- common/job.king.js
+++ common/job.king.js
@@ -1,25 +1,92 @@
+var config = require('./config.js');  
+var util = require('./util.js');
 var client = require('beanstalk_client').Client;
+var conn = null;
 
-exports.spawnClientToRes = function(doJob)
-{
-console.log('spawning client');
+function connect(tube, doJob, callback)
+{ 
+  util.log({"status":"connecting to beanstalk"});
+  client.connect(config.beanstalk.uri+':11300', function(err, c) 
+  {
+    var msg = err ? {"status":err} : {"status":"connected to beanstalk"};
+    util.log(msg);
+    conn = c;
+    callback(tube, doJob);
+  });  
+}
 
-client.connect('184.106.78.48:11300', function(err, conn) {
-  conn.watch('ws_deliver_to_client', function() {
-     conn.reserve(function(err, job_id, job_json) {
+exports.spawnClientToRes = function(tube, doJob)
+{ 
+  if (!conn)
+  {
+    connect(tube, doJob, exports.spawnClientToRes);
+    return;
+  }   
+  
+  conn.watch(tube, function() 
+  {
+    conn.reserve(function(err, job_id, job_json) 
+    {
       console.log('got job: ' + job_id);
+     
+      doJob(JSON.parse(job_json), function()
+      {
+        conn.destroy(job_id, function(rr) 
+        {
+          console.log('destroyed job');
+          exports.spawnClientToRes(tube, doJob);
+          //conn=null...not deleting current connection
+        });
+      });
+    });
+  });
+}
 
+//TODO...
+exports.spawnClientToPut = function(tub, job)
+{
+  console.log('spawning client');
+  client.connect(config.beanstalk.uri+':11300', function(err, conn) 
+  {
+    conn.use(tube, function() 
+    {
+      conn.put(0, 0, 1, JSON.stringify(job), function(err, job_id) 
+      {
+        console.log('added job: ' + job_id);
+        conn = null;
+      });
+    });
+  });  
+}
+
+exports.spawnClientToRes(config.websocket.tube, function(job, callback)
+{
+  console.log(job);
+  callback();
+});
+
+/*
+exports.spawnClientToRes = function(tube, doJob)
+{
+  console.log('spawning client');
+  client.connect(config.beanstalk.uri+':11300', function(err, conn) 
+  {
+    conn.watch(tube, function() 
+    {
+      conn.reserve(function(err, job_id, job_json) 
+      {
+        console.log('got job: ' + job_id);
         doJob(JSON.parse(job_json), function()
         {
-          conn.destroy(job_id, function(err) {
-          console.log('destroyed job');
-          exports.spawnClientToRes(doJob);
-          conn=null;
+          conn.destroy(job_id, functionion(err) 
+          {
+            console.log('destroyed job');
+            exports.spawnClientToRes(tube, doJob);
+            conn=null;
           });
         });
       });
-
-     });
     });
+  });
 }
-
+*/  
