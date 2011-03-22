406223ce7da7b8b2df737bd5f29c45fedf61fa7e
mkrecny <mkrecny@gmail.com>
2011-03-22 16:15:21 -0400

    new backfill

--- twitter/backfill.js
+++ twitter/backfill.js
@@ -1,34 +1,36 @@
+//profiler///
+//var profiler = require('v8-profiler');
+//profiler.startProfiling('startup');
 //config//
 var config = require('../common/config.js');
 //util//
 var util = require('../common/util.js');
 //twitter//
 var twitter = require('./lib/node-twitter/lib/twitter.js');
-//redis//
-//var redis = require('redis').createClient(config.redis_config.port, config.redis_config.server);
 //beanstalkd//
-var job_manager = require('../common/job.manager.js');
+var jobber = require('../common/beanstalk/jobber.js');
 
 var page_start = 17;
 
 /*
 * Begin the backfill proccess. Named start due to asyncness
 */
-function startBackfill(twit_client, twitter_user_id, job_id)
-{
-  
+function startBackfill(twit_client, twitter_user_id, deleteJobAndListen)
+{  
   var page_counter = 0;
   
   for(var p=page_start; p>0; p-=1)
   { 
-    util.log({status:'retrieving pages', type:'backfill', job_id:job_id, twitter_id:twitter_user_id})
+    util.log({status:'retrieving pages', type:'backfill', twitter_id:twitter_user_id})
     
     getPage(twit_client, p, function(page)
-    {
+    { 
       page_counter+=1;
+      
       if (page_counter==17) 
-      {
-	      gotAllPages(twit_client, job_id);
+      { 
+        twit_client = null;
+        deleteJobAndListen();
       }
       
       getPageLinks(page, function(link, tweet)
@@ -52,9 +54,9 @@ function addLinkToQueue(link, tweet, twitter_id)
      "provider_user_id":twitter_id
   };
 
-  job_manager.addJob(config.twitter_link_tube, job_spec, function(job_data)
+  jobber.putJob(job_spec, function(data)
   {
-    util.log({status:'link proccess job added', job_id:job_data}); 
+    //do nothing
   });
 }
 
@@ -80,35 +82,20 @@ function getPageLinks(page, linkExtractedCallback)
     if (page[i] && page[i].entities && page[i].entities.urls && page[i].entities.urls.length)
     { 
         var url = page[i].entities.urls[0].expanded_url ? page[i].entities.urls[0].expanded_url : page[i].entities.urls[0].url;
-        util.expandURL(url, page[i], function(expanded, tweet)
-        {
-          linkExtractedCallback(expanded, tweet);    
-        });
+        linkExtractedCallback(url, page[i]);
     }
   }
 }
 
 /*
-* A callback when all pages for a given job have been retreived
-*/
-function gotAllPages(twit_client, job_id)
-{
-  util.log({status:'all pages retrieved', type:'backfill', "job_id":job_id});
-  job_manager.deleteJob(job_id, function(job_del_data)
-  { 
-    listen();
-  });
-}
-
-/*
 * Get Twitter Client and pass it to startBackfill
 */
-function addUser(job_id, job_data)
+function addUser(job_data, deleteJobAndListen)
 {
-  util.log({status:'commencing new job', type:'backfill', job_id:job_id, twitter_id:job_data.twitter_id});
+  util.log({status:'commencing new job', type:'backfill', twitter_id:job_data.twitter_id});
   initTwitterClient(job_data, function(twitter_client)
   {
-    startBackfill(twitter_client, job_data.twitter_id, job_id);
+    startBackfill(twitter_client, job_data.twitter_id, deleteJobAndListen);
   });
 }
 
@@ -117,7 +104,8 @@ function addUser(job_id, job_data)
 */
 function initTwitterClient(job_data, _startBackfill)
 {
-  var twit_cfg = config.twitter_keys;
+  //var twit_cfg = config.twitter_keys;
+  var twit_cfg = {};
   twit_cfg.access_token_key = job_data.oauth_token;
   twit_cfg.access_token_secret = job_data.oauth_secret;
   var twitter_client = new twitter(twit_cfg);
@@ -127,28 +115,23 @@ function initTwitterClient(job_data, _startBackfill)
 /*
 * Switch for the job action and execute the corresponding function
 */
-function proccessNewJob(job)
-{
-  var job_data = eval('(' + job.data + ')');
-  console.log(job_data);
-  switch(job_data.action)
-  {
+function proccessNewJob(job, deleteJob)
+{ 
+  
+  job.data = JSON.parse(job.data);
+  switch(job.data.action)
+  { 
     case 'add_user':
-    addUser(job.id, job_data);
+    addUser(job.data, deleteJob);
     break;
   }
-}
+} 
 
-/*
-* Listen for new add_user jobs
-*/
-function listen()
+//job_king.init(config.twitter_backfill_tube, proccessNewJob, config.twitter_link_tube, job_king.spawnClientToRes);
+jobber.init(10, config.twitter_backfill_tube, config.twitter_link_tube, function()
 {
-  job_manager.listenForJobs(config.twitter_backfill_tube, proccessNewJob);
-}
-
-listen();
-
+  jobber.resJob(proccessNewJob)
+});
 
 
 
