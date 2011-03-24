3051ca33a1cfcbb76436881da7a6cfe7476e1377
mkrecny <mkrecny@gmail.com>
2011-03-24 17:46:56 -0400

    make feed test using henrys data

---
+++ facebook/test/make_my_info_henry.js
@@ -0,0 +1,15 @@
+var fb_dao = require('../lib/facebook_dao.js');
+
+fb_dao.setUserInfo('646521614', 
+{"facebook_id":646521614, "access_token":'115071338568035|7f8a444abb8e95d8b73ef2b4-646521614|URDPWTAwEsDCBIrnRdrjhKFgBf8', "last_seen":0},
+function(err, res)
+{
+  console.log(err+" : "+res);
+});
+
+
+//646521614
+//115071338568035|7f8a444abb8e95d8b73ef2b4-646521614|URDPWTAwEsDCBIrnRdrjhKFgBf8
+
+//1319152
+//115071338568035|cf4da0397104a7a50b4cda8b-1319152|hymtzZncfOtETKwLh84fnI7y4ZE
