e67257e87f7de36e2c627479b7bbed1dfa522394
mkrecny <mkrecny@gmail.com>
2011-03-24 10:01:00 -0400

    log management tools

---
+++ common/tools/log.manager.js
@@ -0,0 +1,16 @@
+var spawn = require('child_process').spawn;
+
+function truncateLogs()
+{
+  var trunc = spawn('truncate', ['--size', '0', 
+  '/var/log/shelby/backfill.log', 
+  '/var/log/shelby/websocket.log', 
+  '/var/log/shelby/stream.log',
+  'redis.log']);
+
+  trunc = null;  
+  
+  console.log('files trunced');
+}
+
+setInterval(truncateLogs, 60 * 10 * 1000);
