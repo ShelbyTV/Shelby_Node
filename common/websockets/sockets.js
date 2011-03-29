302f530bf317c12237785d0033507e745ec32d79
mkrecny <mkrecny@gmail.com>
2011-03-29 16:14:46 -0400

    having another stab at sockets.js

--- common/websockets/sockets.js
+++ common/websockets/sockets.js
@@ -1,114 +1,99 @@
-var config = require('../config.js');
-var sys  = require('sys');
-var io   = require('socket.io');
-var http = require('http');
-var Clients = {};
-var util = require('../util.js');
-var jobber = require('../beanstalk/jobber.js');
+var config = require('../config.js'),
+sys  = require('sys'),
+io   = require('socket.io'),
+http = require('http'),
+Clients = {},
+util = require('../util.js'),
+jobber = require('../beanstalk/jobber.js');
 
-server = http.createServer(function(req, res) 
-{ 
-  res.writeHead(200, {'Content-Type': 'text/html'}); 
-  res.end(); 
-});
-
-server.listen(config.websocket.server_port, config.websocket.server_uri);
-console.log('socket webserver listening on '+config.websocket.server_uri+':'+config.websocket.server_port)
-
-var socket = io.listen(server); 
-
-socket.on('connection', function(client){ 
-	
-	client.on('message', function(message)
-	{ 
-	  console.log(message);
-	  switch(message.action)
-	  {
-	    case 'init':
-	    addNewClient(client, message.user_id, function()
-	    {
-	     util.log({"status":'adding client', "user_id":message.user_id});
-	     logAllClients();
-	    });
-	    break;
-	  }
-	});  
-	
-	client.on('disconnect', function()
-	{  
-	  removeClient(client, function()
-	  {
-	    util.log({"status":"removing client", "user_id":client.user_id});
-	    logAllClients();
-	  });
-	});
-	
-});
-
-function removeClient(client, callback)
-{
-  delete Clients[client.user_id];
-  logAllClients();
-  callback();
-}
-
-function addNewClient(client, user_id, callback)
-{
-  client.user_id = user_id;
-  Clients[user_id] = client;
-  callback();
-}
-
-function pushPayloadToClient(payload, user_id, callback)
-{
- if (!Clients[user_id])
- {
-   util.log({"status":"user "+user_id+" not currently connected"});
- } 
- else
- { 
-   
-   util.log({"status":"sending payload to user", "client":user_id});
-   Clients[user_id].send(payload);    
- }
- callback();
-  
-}
-
-function logAllClients()
-{ 
+function logAllClients(){ 
   var ids = [];
-  
-  for (var i in Clients)
-  {
-    ids.push(i);
-  }
-  
+  for (var i in Clients){
+    if (Clients.hasOwnProperty(i)){
+      ids.push(i);
+    }
+  }  
   util.log('ALL CLIENTS:');
   util.log(ids);
-  delete ids;
+  ids = null;
 }
 
-function proccessNewJob(job, deleteJobAndListen)
-{ 
-  job = JSON.parse(job);
-  job = JSON.parse(job.data);  
-//util.log(job.data);  
-//job = eval('(' + job.data + ')');
-  if (job.payload && job.user_id)
-  {  
-    pushPayloadToClient(job.payload, job.user_id, deleteJobAndListen);
-  }
-  else
-  {
-      util.log({"status":"bad/null job data"});
-      deleteJobAndListen();
-  }
+function WSManager(){
+  
+  var self = this;
+  
+  this.removeClient = function(client, callback){
+    if (!client.hasOwnProperty('user_id')){
+      return;
+    }
+    util.log({"status":"removing client", "user_id":client.user_id});
+    delete Clients[client.user_id];
+    logAllClients();
+    return callback();
+  };
+
+  this.addNewClient = function(client, user_id, callback){
+    client.user_id = user_id;
+    Clients[user_id] = client;
+    util.log({"status":'adding client', "user_id":client.user_id});
+    return callback();
+  };
+
+  this.pushPayloadToClient = function(payload, user_id, callback){
+    if (!Clients.hasOwnProperty(user_id)){
+      util.log({"status":"user "+user_id+" not currently connected"});
+    } else { 
+      util.log({"status":"sending payload to user", "client":user_id});
+      Clients[user_id].send(payload);    
+    } 
+    return callback();
+  };
+
+  this.proccessNewJob = function(job, deleteJobAndListen){ 
+    job.data = JSON.parse(job.data);  
+
+    if (job.data.payload && job.data.user_id){  
+      return self.pushPayloadToClient(job.data.payload, job.data.user_id, deleteJobAndListen);
+    } else {
+        util.log({"status":"bad/null job data"});
+        return deleteJobAndListen();
+    }
+  };
+  
+  this.init = function(){
+    var server = http.createServer(function(req, res) { 
+      res.writeHead(200, {'Content-Type': 'text/html'}); 
+      res.end(); 
+    });
+
+    //server.listen(config.websocket.server_port, config.websocket.server_uri);
+    server.listen(5666, config.websocket.server_uri);
+    
+    console.log('socket webserver listening on '+config.websocket.server_uri+':'+config.websocket.server_port);
+
+    var socket = io.listen(server); 
+
+    socket.on('connection', function(client){ 
+      client.on('message', function(message){ 
+        console.log(message);
+        if (message.action=='init'){
+          self.addNewClient(client, message.user_id, function(){
+            return logAllClients();
+          });
+        }
+      });  
+
+      client.on('disconnect', function(){  
+        self.removeClient(client, function(){});
+      });
+    });
+
+    jobber.init(10, config.websocket.tube, null, function(){ 
+      jobber.resJob(self.proccessNewJob);
+    });  
+  }; 
 }
 
-//jobber.init(config.websocket.tube, proccessNewJob, null, job_king.spawnClientToRes);
+var ws = new WSManager();
+ws.init();
 
-jobber.init(10, config.websocket.tube, null, function()
-{ 
-  jobber.resJob(proccessNewJob);
-});
